d8973639a69a7fa4afe3b6b4050a1d390cf5cc48
Ansari Rumaesa <rumaesaansari@gmail.com>
2025-03-05 15:27:49 +0530

    created a basic spring boot project structure with product and product-category repository

---
+++ src/main/java/com/sg/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,29 @@
+package com.sg.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.sg.ecommerce.entity.Product;
+import com.sg.ecommerce.entity.ProductCategory;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		HttpMethod[] theUnsupportedActions = { HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE };
+		
+//	disable HTTP Methods for product: PUT, POST, DELETE
+		config.getExposureConfiguration().forDomainType(Product.class)
+				.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+				.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+		
+//	disable HTTP Methods for productCategory: PUT, POST, DELETE
+			config.getExposureConfiguration().forDomainType(ProductCategory.class)
+					.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+					.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+	}
+}
